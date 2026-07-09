f37de3ccbd780c6143d886a28cf535d2434ad368
Ujjwal Kumar <krujjwal0651@gmail.com>
2026-07-09 14:48:42 +0530

    test2

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -63,5 +63,5 @@ resource "aws_vpc_security_group_ingress_rule" "web_sg_allow_https_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "web_sg_allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.web_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" ## semantically equivalent to all ports
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
